4fe722eaf61808127205f8bbc38826d4c4826bb0
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2023-05-13 00:38:57 +0300

    solve Bayes rule error

--- Bayes.py
+++ Bayes.py
@@ -5,12 +5,8 @@ import math
 from Training import load_data
 
 def multivariate_normal_gaussian(X, mu, sigma):
-    det = np.linalg.det(sigma)
-    norm_const = 1 / (((2*np.pi) ** (mu.shape[0]/2)) * (det ** 0.5) )
-    x_mu = X - mu
-    inv = np.linalg.inv(sigma)
-    result = np.exp(-0.5 * (x_mu.T @ inv @ x_mu))
-    prob = norm_const * result
+    z,_=sigma.shape
+    prob = (1/(((2*np.pi)**(z/2))*(np.linalg.det(sigma)**0.5)))* np.exp(-0.5*(np.dot(np.dot((X-mu).T,np.linalg.inv(sigma)),(X-mu))))
     return prob
 
 print("hello bayes before load")
@@ -21,51 +17,45 @@ numClasses = 6
 M = len(Xtrain)
 N = len(Xtrain[0]) - 1
 K = len(Xtest)
-
+ytrain=np.asarray(ytrain)
 print("hello bayes")
 
-pClasses = [] # A list of size (numClasses, 1) containing the a priori probabilities of each class in the training set.
+pClasses =np.zeros((numClasses, 1)) # A list of size (numClasses, 1) containing the a priori probabilities of each class in the training set.
 
-estimate_means = [] # A numpy array of size (numClasses, N) containing the mean points of each class in the training set. 
+estimate_means = np.zeros((numClasses,N)) # A numpy array of size (numClasses, N) containing the mean points of each class in the training set. 
                     # HINT: USE NP.MEAN
 
-estimate_covariances = [] # A numpy array of size (numClasses, N, N) containing the covariance matrices of each class in the training set.
+estimate_covariances = np.zeros((numClasses,N,N)) # A numpy array of size (numClasses, N, N) containing the covariance matrices of each class in the training set.
                           # HINT: USE NP.COV (Pay attenention for what it takes as an argument)
 
 for classIndex in range(numClasses):
     # TODO [5]: Estimate the parameters of the Gaussian distributions of the given classes.
     # Fill pClasses, estimate_means, and estimate_covariances in this part 
     # Your code should be vectorized WITHOUT USING A SINGLE FOR LOOP.
-    pClasses.append(len(ytrain[np.where(ytrain == classIndex+1)])/len(ytrain))
-    
-    x = np.mean(Xtrain[np.where(ytrain == classIndex+1)] , axis=0)
-    estimate_means.append(x)
-    
-    x = np.cov(Xtrain[np.where(ytrain == classIndex+1)].T)
-    estimate_covariances.append(x)
-    
-
+    pClasses[classIndex] = np.count_nonzero(ytrain == (classIndex+1))
+    mask = (Xtrain[:, 0] == classIndex+1)
+    estimate_means[classIndex]=np.mean(Xtrain[mask, 1:],axis=0)
+    estimate_covariances[classIndex]=np.cov(Xtrain[mask, 1:],rowvar=False)
+pClasses=pClasses/(len(ytrain))
 estimate_means = np.array(estimate_means)
 estimate_covariances = np.array(estimate_covariances)
 
 
 
-
 predicted_classes = np.zeros((K,1)) # predicted_classes: A numpy array of size (K, 1) where K is the number of points in the test set. Every element in this array
-                       # contains the predicted class of Bayes classifier for this test point.
+                                    # contains the predicted class of Bayes classifier for this test point.
 
 for i in range(Xtest.shape[0]):
-    print("For test point:", Xtest[i])
     classProbabilities = np.zeros(numClasses)
     # TODO [7.A]: Compute the probability that the test point X_Test[i] belongs to each class in numClasses.
     #  Fill the array classProbabilities accordingly.
     for j in range(3):    
-        classProbabilities[j] = ( multivariate_normal_gaussian(Xtest[i], estimate_means[j], estimate_covariances[j]))
-        
+        classProbabilities[j] = multivariate_normal_gaussian(Xtest[i],estimate_means[j],estimate_covariances[j])*pClasses[j]
+
     # TODO [7.B]: Find the prediction of the test point X_Test[i] and append it to the predicted_classes array.
     predicted_classes[i]=np.argmax(classProbabilities)+1
     
 
 
-accuracy = len(np.where(ytest == predicted_classes)[0]) / len(predicted_classes)
+accuracy = ( ( predicted_classes == ytest ).sum() )/ len(ytest)
 print('Accuracy = ' + str(round(accuracy,4) * 100) + '%')
