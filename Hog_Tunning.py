c79450da0cc3c2fbb31dd1f9e003abb9e0c85759
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2023-05-13 18:48:47 +0300

    update hog tuning code

--- Hog_Tunning.py
+++ Hog_Tunning.py
@@ -1,40 +1,43 @@
+from sklearn.model_selection import GridSearchCV
 from skimage.feature import hog
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score
+from utils import load_data
 import numpy as np
 
-from utils import load_data
-# Load the hand gesture data and split into training and validation sets
+class HogTransformer():
+    def __init__(self, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3, 3)):
+        self.orientations = orientations
+        self.pixels_per_cell = pixels_per_cell
+        self.cells_per_block = cells_per_block
 
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return np.array([hog(x.reshape((28, 28)), orientations=self.orientations, pixels_per_cell=self.pixels_per_cell,
+                             cells_per_block=self.cells_per_block) for x in X])
 X_train, X_val, y_train, y_val = load_data(directory='./Dataset/')
 
-# Define the range of parameter values to test
-cell_sizes = [(8, 8), (16, 16)]
-block_sizes = [(1, 1), (2, 2), (4, 4)]
-n_bins = [6, 9, 12]
+# Define the parameter grid to search over
+param_grid = {
+    'orientations': [8, 9, 10],
+    'pixels_per_cell': [(8, 8), (10, 10), (12, 12)],
+    'cells_per_block': [(2, 2), (3, 3)]
+}
 
-# Define the SVM classifier
+# Create a pipeline with the hog feature extractor and the SVM classifier
+pipeline = Pipeline([
+    ('hog', HogTransformer()),
+    ('svm', SVC())
+])
 
-svm = SVC(kernel= 'poly', gamma= 100.0, degree= 4, coef0= 0, class_weight= 'balanced', C= 0.1)
+# Create a grid search object with 5-fold cross-validation
+grid_search = GridSearchCV(pipeline, param_grid, cv=5, n_jobs=-1)
 
-# Define the parameter grid for GridSearchCV
-param_grid = {'hog__cell_size': cell_sizes,
-              'hog__block_size': block_sizes,
-              'hog__orientations': n_bins,
-              'svm__C': [0.1, 1, 10, 100],
-              'svm__gamma': ['scale', 'auto'] + list(np.logspace(-3, 3, 7))}
-
-# Define the pipeline for combining HOG feature extraction and SVM classification
-pipeline = Pipeline([('hog', hog()),
-                     ('svm', svm)])
-
-# Define the GridSearchCV object and fit to the training data
-grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=3, verbose=2)
+# Fit the grid search object to the training data
 grid_search.fit(X_train, y_train)
 
-# Print the best parameters and model accuracy on the validation data
-print(grid_search.best_params_)
-y_pred = grid_search.predict(X_val)
-print(accuracy_score(y_val, y_pred))
+# Print the best parameters and the corresponding score
+print("Best parameters:", grid_search.best_params_)
+print("Best score:", grid_search.best_score_)
