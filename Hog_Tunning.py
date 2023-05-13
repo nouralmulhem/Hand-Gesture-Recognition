c5de3a2c348728bdc6af7d01dfe30339b9da8810
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2023-05-13 18:16:48 +0300

    update hog tuning code

--- Hog_Tunning.py
+++ Hog_Tunning.py
@@ -1,92 +1,40 @@
-# import os
-# import numpy as np
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.svm import SVC
-# from sklearn.pipeline import Pipeline
-# from skimage.feature import hog
-# from utils import * 
-
-# # Define the parameters to search over
-# param_grid = {
-#     'hog__orientations': [9, 10, 11],
-#     'hog__pixels_per_cell': [(8, 8), (12, 12)],
-#     'hog__cells_per_block': [(2, 2), (3, 3)]
-# }
-
-# # Load images from a folder
-# X = []
-# y = []
-
-# y,X = obtain_images('./Dataset/')
-# # for root, dirs, files in os.walk('path/to/folder'):
-# #     for file in files:
-# #         if file.endswith('.jpg'):
-# #             image = cv2.imread(os.path.join(root, file))
-# #             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# #             resized = cv2.resize(gray, (64, 128))
-# #             X.append(resized)
-# #             y.append(root.split('/')[-1])  # use the folder name as the class label
-
-# # Convert the images to HOG features
-# hog_pipeline = Pipeline([
-#     ('hog', hog(
-#         orientations=9,
-#         pixels_per_cell=(8, 8),
-#         cells_per_block=(2, 2),
-#         visualize=False,
-#         multichannel=False
-#     ))
-# ])
-# X_hog = hog_pipeline.fit_transform(X)
-
-# # Define the SVM classifier
-# svm = SVC(kernel='linear', C=1)
-
-# # Perform GridSearchCV to find the best parameters
-# grid_search = GridSearchCV(
-#     estimator=svm,
-#     param_grid=param_grid,
-#     cv=5,
-#     n_jobs=-1
-# )
-# grid_search.fit(X_hog, y)
-
-# # Print the best parameters
-# print("Best parameters: ", grid_search.best_params_)
-
-
-
 from skimage.feature import hog
-from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
-from utils import *
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+import numpy as np
 
+from utils import load_data
+# Load the hand gesture data and split into training and validation sets
 
+X_train, X_val, y_train, y_val = load_data(directory='./Dataset/')
 
-# Define a grid of parameter values to search over
-param_grid = {
-    'orientations': [6, 9, 12],
-    'pixels_per_cell': [(8, 8), (16, 16)],
-    'cells_per_block': [(1, 1), (2, 2)]
-}
+# Define the range of parameter values to test
+cell_sizes = [(8, 8), (16, 16)]
+block_sizes = [(1, 1), (2, 2), (4, 4)]
+n_bins = [6, 9, 12]
 
-Xtrain, Xtest, ytrain, ytest = load_data(directory='./Dataset/')
+# Define the SVM classifier
 
-# Train an SVM classifier on the HOG features using grid search
-svm = SVC(kernel='rbf', C=1, gamma='scale')
-grid_search = GridSearchCV(svm, param_grid, cv=5, n_jobs=-1)
-grid_search.fit(Xtrain, ytrain)
+svm = SVC(kernel= 'poly', gamma= 100.0, degree= 4, coef0= 0, class_weight= 'balanced', C= 0.1)
 
-# Print the best parameters and validation set performance
-print(f"Best parameters: {grid_search.best_params_}")
-print(f"Validation accuracy: {grid_search.best_score_}")
+# Define the parameter grid for GridSearchCV
+param_grid = {'hog__cell_size': cell_sizes,
+              'hog__block_size': block_sizes,
+              'hog__orientations': n_bins,
+              'svm__C': [0.1, 1, 10, 100],
+              'svm__gamma': ['scale', 'auto'] + list(np.logspace(-3, 3, 7))}
 
-# Evaluate the performance of the chosen parameters on the test set
-X_test_hog = []
-for img in Xtest:
-    feature_vector, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True)
-    X_test_hog.append(feature_vector)
-X_test_hog = np.array(X_test_hog)
-y_test_pred = grid_search.predict(X_test_hog)
-accuracy = accuracy_score(ytest, y_test_pred)
-print(f"Test accuracy: {accuracy}")
+# Define the pipeline for combining HOG feature extraction and SVM classification
+pipeline = Pipeline([('hog', hog()),
+                     ('svm', svm)])
+
+# Define the GridSearchCV object and fit to the training data
+grid_search = GridSearchCV(pipeline, param_grid=param_grid, cv=3, verbose=2)
+grid_search.fit(X_train, y_train)
+
+# Print the best parameters and model accuracy on the validation data
+print(grid_search.best_params_)
+y_pred = grid_search.predict(X_val)
+print(accuracy_score(y_val, y_pred))
--- svm_randomesearch.py
+++ svm_randomesearch.py
@@ -25,10 +25,11 @@ random_grid = {'C': C,
                'class_weight': class_weight}
 
 # Perform Randomized Search to find the best hyperparameters
-svm_random = RandomizedSearchCV(estimator = svm, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+svm_random = RandomizedSearchCV(estimator = svm, param_distributions = random_grid, n_iter = 400, cv = 5, verbose=2, random_state=42, n_jobs = -1)
 svm_random.fit(X_train, y_train)
 
 # Print the best hyperparameters and model accuracy
 print(svm_random.best_params_)
 print(svm_random.best_score_)
 print(svm_random.score(X_test, y_test))
+# {'kernel': 'poly', 'gamma': 100.0, 'degree': 4, 'coef0': 0, 'class_weight': 'balanced', 'C': 0.1}
