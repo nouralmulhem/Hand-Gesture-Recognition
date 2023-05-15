5bcb8d72c959ba4d87d1ebf7656a42e5cd5c445d
ahmedmadbouly186 <66012617+ahmedmadbouly186@users.noreply.github.com>
2023-05-15 17:25:40 +0300

    n_itr=250

--- randomforest_ranodmsearch.py
+++ randomforest_ranodmsearch.py
@@ -7,9 +7,9 @@ from utils import load_data
 X_train, X_test, y_train, y_test = load_data(directory='./Dataset/')
 
 # Define the hyperparameter space for Random Forest
-n_estimators = [int(x) for x in np.linspace(start = 50, stop = 1000, num = 10)]
+n_estimators = [int(x) for x in np.linspace(start=50, stop=1000, num=10)]
 max_features = ['sqrt', 'log2', None]
-max_depth = [int(x) for x in np.linspace(5, 110, num = 11)]
+max_depth = [int(x) for x in np.linspace(5, 110, num=11)]
 max_depth.append(None)
 min_samples_split = [2, 5, 10]
 min_samples_leaf = [1, 2, 4]
@@ -27,7 +27,8 @@ random_grid = {'n_estimators': n_estimators,
                'bootstrap': bootstrap}
 
 # Perform Randomized Search to find the best hyperparameters
-rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 400, cv = 5, verbose=2, random_state=42, n_jobs = -1)
+rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid,
+                               n_iter=250, cv=5, verbose=2, random_state=42, n_jobs=-1)
 rf_random.fit(X_train, y_train)
 
 # Print the best hyperparameters and model accuracy
