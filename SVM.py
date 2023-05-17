820d3ff4177cc999735be947aa7f69c70ea05d13
Nour Almulhem <nouralmulhem@gmail.com>
2023-05-17 17:51:05 +0300

    86

--- SVM.py
+++ SVM.py
@@ -15,7 +15,7 @@ def svm_model(X_train, X_test, y_train):
 
     # Create an SVM model
     # svm = SVC(kernel='rbf', C=1, random_state=0)
-    svm = SVC(kernel='poly', C=0.1, random_state=0, coef0=-1, degree=4, gamma=10)
+    svm = SVC(kernel='poly', C=1, random_state=0, coef0=0, degree=4, gamma='scale')
 
     # Train the SVM model on the training set
     svm.fit(X_train, y_train)
